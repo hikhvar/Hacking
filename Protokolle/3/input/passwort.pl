95a969ffdfb5a000b8bc1d5fcbb1d2ebb88bc313
hikhvar <crobbert@mail.upb.de>
2014-01-16 11:31:16 +0100

    Some cleanup.

---
+++ Protokolle/3/input/passwort.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl 
+@@=split(/x3a/,"v:s:S:P"); 
+$junk=("j","u","n","k","?"); 
+@==split(//,"feedback"); 
+$__="ciu3fupaebf3urq3zbghiunti6eznuw5sngsnfonoefce"; 
+$zZzZzz=("w00t?"); 
+@zZzZzz=split(//,$zZzZzz); 
+print "Was ergibt es wohl im Ganzen?n"; 
+######################## BERNDS 8 BRUCHSTUECKE ###################### 
+sub _{$ad = 
+""G33k"";@a=map{ord(uc(chr(ord($_)+ord($_))))}substr($ad,1,1);&__(@a);} ($junk[5]=~/?/)?(@res1=ord('-')):(@res1=map ord($_), @@[&_+2]);${pack 'H*','7061727431'}=@res1[0].'a7'; sub __{return @a[0]+@a[1]+ord(substr($ad,0,1))-ord(substr($ad,3,1))-ord(substr($ad,4,1));} 
+%_h = (a => 4,b => 7, c => 3,d => 9,e => 7,f => 5,g => 5,h => 3,i => 9,j => 1,k => 3,l => 1,m => 7,n => 3,o => 9,p => 2,q => 1,r => 6,s => 3,t => 8,u => 3,v => 1,w => 9,x => 9,y => 7,z => 3);foreach $_k (keys % _h) {($_h{$_k}%2!=0?($_7[9]=2):($_7[$_h{$_k}]=$_k));}${join('',@_7)}=@=[6].@=[1].&__2;sub __2{eval(scalar reverse (" nruter").ord(uc('v')));} 
+$cmd = "cat tmp | grep art345";&___; sub ____ {${substr(($cmd),10,5)} = @QQQ[0].@QQ[0].@QQ[1].@QQQ[1];}@QQ = split(//,((map ord($_), @@[&_+3])[0]));@QQQ=split(//,sprintf("%x",32));sub ___{$cmd =~ s/ //g;}&____; 
+${scalar reverse('trap').(16%12)} = chr(ord(lc('G'))-1).(map ord($_), @@[&_+1])[0]; 
+$t1  = &_sub_.int(100/eval(`uname -r|rev|tr -d 'HCRA'|tr '-' '+'|tr '.' '/'`));&_sub;sub _sub_{&_sub_sub_("ddea96c5e0afb6d5d3ca3b5b5s9c37fd" =~ /[0-9a-f]{32}/?5:4)}sub d{$i=0;@i;@w00t=(7,-49,-66,0,10);foreach(@_){push(@i,chr(ord($_)-@w00t[$i]));$i++;}return join('',@i);}sub _sub {${&d(@zZzZzz)} = $t1."e";}sub _sub_sub_{return 33%$_[0];} 
+$qe="leer";sub O0O0 {return ($_[0]>$_[1])?$_[0]:$_[1];}sub O0OO {return ($_[0]>$_[1])?$_[1]:$_[0];}&nn;${$__}=substr($qe,3,1).substr($qe,0,1).substr($qe,1,1).substr($qe,2,1);sub mm{$b=0;$c=9;for ($j = 1022; $j<=1039; $j++) {$b = &O0O0($b,int(''.(split(//,$j))[2]));$c = &O0OO($c,int(''.(split(//,$j))[3]))+0;}push(@d,$b);push(@d,$c);return @d;}sub nn{$qe = join((&mm)[0],chr(ord(@=[7])-8).@=[3],(&mm)[1]);$__=~s/[b-oqsu-z0-458-97]//g;} 
+${&_b("1110000110000111100101110100110111")}="fa26";sub _b{@_4 = split(//,@_[0]);for ($i = 0; $i < scalar(@_4); $i++) {push (@_5,@_4[$i]);(($i+1)%7==0)||($i==scalar(@_4)-1)?push(@_6,chr(_bd_(join('',@_5)))):0;(($i+1)%7)==0?splice(@_5,0):0;}return join('',@_6);}sub _bd_{unpack("N", pack("B32", substr("0" x 32 . shift, -32)));} 
+@we = ("v","g","x","z");sub rot {$rt=undef;$n=pop(@_);foreach (@_) {$rt.=chr(ord($_)+$n);}return $rt;}${&rot(@we,-6).(2^2?1:2**3)} = (&tract)[0].(&tract)[1].(&tract)[2].@=[4];sub tract {push(@",1);for ($^^=1;$^^<=2;$^^++) {$^^%2!=0?(push(@",$^^+$^^)):(push(@",$^^+(-1)*$^^));}return @";} 
+#####################################################################
